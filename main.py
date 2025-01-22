12e96935776621a80d1ded9b323a24121cf7e4af
Ichwan <ichwan.laruku@gmail.com>
2025-01-22 15:26:00 +0700

    Add sidebar menu for data input and dashboard display

--- main.py
+++ main.py
@@ -25,26 +25,50 @@ worksheet = sh.sheet1
 data = worksheet.get_all_records()
 df = pd.DataFrame(data)
 
-# Form inputs
-st.write('Input Data Google Sheets')
-with st.form('input_form'):
-    tanggal = st.date_input('Tanggal')
-    kelas = st.selectbox('Kelas', 'Kelas-1A Kelas-1B Kelas-1C Kelas-1D'.split())
-    jumlah = st.select_slider('Jumlah Sepatu yang Tertata Rapi', options=range(1, 28))
-    submit_button = st.form_submit_button(label='Submit Data')
+menu = st.sidebar.radio(
+    'Menu', 
+    ['Dashboard', 'Input Data']
+)
 
-# Submit data to Google Sheets
-if submit_button:
-    if kelas and jumlah:
-        # convert tanggal to string
-        tanggal_str = tanggal.strftime('%Y-%m-%d')
+if menu == 'Dashboard':
+    # Display the data
+    st.write('Data from Google Sheets')
+    st.dataframe(df)
 
-        # append data to Google Sheets
-        worksheet.append_row([tanggal_str, kelas, jumlah])
-        st.success('Data berhasil ditambahkan ke Google Sheets')
-    else:
-        st.error('Data tidak boleh kosong')
+    # Display the summary
+    st.write('Summary')
+    st.write(df.describe())
 
-# Display the data
-st.write('Data from Google Sheets')
-st.dataframe(df)
+ 
+
+elif menu == "Input Data":
+    # Form inputs
+    st.write('Input Data Google Sheets')
+    with st.form('input_form'):
+        tanggal = st.date_input('Tanggal')
+        kelas = st.selectbox('Kelas', 'Kelas-1A Kelas-1B Kelas-1C Kelas-1D'.split())
+        jumlah_rapi = st.select_slider('Jumlah Sepatu yang Tertata Rapi', options=range(1, 28))
+        submit_button = st.form_submit_button(label='Submit Data')
+        
+        # Submit data to Google Sheets
+        if submit_button:
+            if kelas and jumlah_rapi:
+                # convert tanggal to string
+                tanggal_str = tanggal.strftime('%Y-%m-%d')
+
+                if kelas == 'Kelas-1A':
+                    jumlah_siswa = 27
+                elif kelas == 'Kelas-1B':
+                    jumlah_siswa = 28
+                elif kelas == 'Kelas-1C':
+                    jumlah_siswa = 29
+                elif kelas == 'Kelas-1D':
+                    jumlah_siswa = 30
+
+                target = jumlah_rapi / jumlah_siswa
+                
+                # append data to Google Sheets
+                worksheet.append_row([tanggal_str, kelas, jumlah_rapi, jumlah_siswa, target])
+                st.success('Data berhasil ditambahkan ke Google Sheets')
+            else:
+                st.error('Data tidak boleh kosong')
