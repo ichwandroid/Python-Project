2f8ac052b6e2c36201c472fc09029511fff3929d
Ichwan <ichwan.laruku@gmail.com>
2025-01-23 14:09:04 +0700

    Enhance dashboard with tabbed interface for data visualization, input, editing, and deletion

--- main.py
+++ main.py
@@ -26,55 +26,69 @@ worksheet = sh.sheet1
 data = worksheet.get_all_records()
 df = pd.DataFrame(data)
 
-menu = st.sidebar.radio(
-    'Menu', 
-    ['Dashboard', 'Input Data']
-)
+st.title('Selamat Datang di Dashboard Sepatu Rapi :chart_with_upwards_trend:')
+st.write('Laporan Jumlah Sepatu yang Tertata Rapi')
+tabChart, tabInput, tabData, tabDelete = st.tabs(['Chart 📊','Input Data ✍️', 'Edit Data 📝', 'Delete Data 🗑️'])
 
-if menu == 'Dashboard':
-    # Display the data
-    st.write('Data from Google Sheets')
-    st.dataframe(df)
+if df.empty:
+    st.warning('Data Masih kosong ⛔')
+else:
 
+    # Display the chart
     source = df
+    tabChart.subheader('Chart Jumlah Sepatu yang Tertata Rapi')
+    tabChart.bar_chart(source, x='Tanggal', y='Jumlah Sepatu Rapi', color='Kelas', stack=False)
+    
+    # Display the data
+    tabData.subheader('Edit Data Jumlah Sepatu yang Tertata Rapi')
+    edited_df = tabData.data_editor(df)
+    if tabData.button('Save Changes'):
+        for i, row in edited_df.iterrows():
+            target_value = row['Jumlah Sepatu Rapi'] / row['Jumlah Siswa']
+            worksheet.update(f'A{i+2}', [[row['Tanggal'], row['Kelas'], row['Jumlah Sepatu Rapi'], row['Jumlah Siswa'], target_value]])
+        st.toast('Data berhasil disimpan ke Google Sheets', icon='📝') 
+    
+    # Display Delete functionality
+    tabDelete.subheader('Delete Data Jumlah Sepatu yang Tertata Rapi')
+    tabDelete.table(df)
+    selected_rows = tabDelete.multiselect('Select rows to delete', edited_df.index)
+    if tabDelete.button('Delete Selected Rows'):
+        if selected_rows:
+            edited_df.drop(selected_rows, inplace=True)
+            # Update Google Sheets after deletion
+            worksheet.clear()  # Clear the existing data
+            worksheet.update([df.columns.values.tolist()] + edited_df.values.tolist())  # Update with new data
+        st.toast('Selected rows deleted and changes saved to Google Sheets', icon='🗑️')
 
-    st.write('Data from Vega Datasets')
-    st.bar_chart(source, x='Tanggal', y='Jumlah Sepatu Rapi', color='Kelas', stack=False)
 
-    # Display the summary
-    st.write('Summary')
-    st.write(df.describe())
+# Display the input form
+tabInput.subheader('Masukkan data jumlah sepatu yang tertata rapi')
+form = tabInput.form('input_form')
+with form:
+    tanggal = st.date_input('Tanggal')
+    kelas = st.selectbox('Kelas', 'Kelas-1A Kelas-1B Kelas-1C Kelas-1D'.split())
+    jumlah_rapi = st.select_slider('Jumlah Sepatu yang Tertata Rapi', options=range(1, 28))
+    submit_button = st.form_submit_button(label='Submit Data')
 
- 
+    # Submit data to Google Sheets
+    if submit_button:
+        if kelas and jumlah_rapi:
+            # convert tanggal to string
+            tanggal_str = tanggal.strftime('%Y-%m-%d')
 
-elif menu == "Input Data":
-    # Form inputs
-    st.write('Input Data Google Sheets')
-    with st.form('input_form'):
-        tanggal = st.date_input('Tanggal')
-        kelas = st.selectbox('Kelas', 'Kelas-1A Kelas-1B Kelas-1C Kelas-1D'.split())
-        jumlah_rapi = st.select_slider('Jumlah Sepatu yang Tertata Rapi', options=range(1, 28))
-        submit_button = st.form_submit_button(label='Submit Data')
-        
-        # Submit data to Google Sheets
-        if submit_button:
-            if kelas and jumlah_rapi:
-                # convert tanggal to string
-                tanggal_str = tanggal.strftime('%Y-%m-%d')
+            if kelas == 'Kelas-1A':
+                jumlah_siswa = 27
+            elif kelas == 'Kelas-1B':
+                jumlah_siswa = 28
+            elif kelas == 'Kelas-1C':
+                jumlah_siswa = 29
+            elif kelas == 'Kelas-1D':
+                jumlah_siswa = 30
 
-                if kelas == 'Kelas-1A':
-                    jumlah_siswa = 27
-                elif kelas == 'Kelas-1B':
-                    jumlah_siswa = 28
-                elif kelas == 'Kelas-1C':
-                    jumlah_siswa = 29
-                elif kelas == 'Kelas-1D':
-                    jumlah_siswa = 30
+            target = jumlah_rapi / jumlah_siswa
 
-                target = jumlah_rapi / jumlah_siswa
-                
-                # append data to Google Sheets
-                worksheet.append_row([tanggal_str, kelas, jumlah_rapi, jumlah_siswa, target])
-                st.success('Data berhasil ditambahkan ke Google Sheets')
-            else:
-                st.error('Data tidak boleh kosong')
+            # append data to Google Sheets
+            worksheet.append_row([tanggal_str, kelas, jumlah_rapi, jumlah_siswa, target])
+            st.toast('Data berhasil ditambahkan ke Google Sheets', icon='🚀')
+        else:
+            st.error('Data tidak boleh kosong')
